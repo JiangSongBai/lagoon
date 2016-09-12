45ff2f5d4676b2c2684e84b687963c43554b5a7f
David García Alonso <g.a.david@gmail.com>
2016-09-12 12:25:18 +0200

    Final Clase 18 - Seccion 3 - Registr Module

--- frontend/app/components/register.component.ts
+++ frontend/app/components/register.component.ts
@@ -8,14 +8,15 @@ import {RegisterService} from '../services/register.service';
   templateUrl: 'app/components/register.component.html'
 })
 export class Register {
-  newUser: User = new User()
+  newUser: User = new User();
+  registered: boolean = false;
 
   constructor(private registerService: RegisterService){}
 
   OnSubmit() {
     this.registerService.sendUser(this.newUser).subscribe(
       data => {
-        //this.registered = true;
+        this.registered = true;
         this.newUser = new User();
       },
       error => console.log(error)
