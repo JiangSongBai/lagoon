e58622f944d2f7d654416577552459502abb63c7
David García Alonso <g.a.david@gmail.com>
2016-09-12 10:40:51 +0200

    Final Clase 17 backend

--- backend/src/main/java/com/ldeng/backend/config/CorsConfig.java
+++ backend/src/main/java/com/ldeng/backend/config/CorsConfig.java
@@ -1,7 +1,31 @@
 package com.ldeng.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 /**
  * Created by d_garcia on 12/09/2016.
  */
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 }
--- backend/src/main/java/com/ldeng/backend/config/JwtFilter.java
+++ backend/src/main/java/com/ldeng/backend/config/JwtFilter.java
@@ -1,7 +1,36 @@
 package com.ldeng.backend.config;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by d_garcia on 12/09/2016.
  */
-public class JwtFilter {
+
+public class JwtFilter { // Java Web Token Filter   --> se debe hacer ya que Angular hace primero un OPTIONS ????
+
+    public void doFilter(final ServletRequest req,
+                         final ServletResponse res,
+                         final FilterChain chain) throws IOException, ServletException {
+
+        final HttpServletRequest  request  = (HttpServletRequest) req;
+        final HttpServletResponse response = (HttpServletResponse) res;
+
+        final String authHeader =  request.getHeader("authorization");
+
+
+        if("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            chain.doFilter(req, res);
+        }
+
+    }
+
 }
+
